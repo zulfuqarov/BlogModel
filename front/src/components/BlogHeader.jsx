af0d1695fec56e8df83a9d6516479c39fd75a30b
zulfuqarov <zulfuqarov.nebi@gmail.com>
2024-05-07 12:26:31 +0400

    Blog about updaiting

--- front/src/components/BlogHeader.jsx
+++ front/src/components/BlogHeader.jsx
@@ -4,27 +4,27 @@ const BlogHeader = ({ data }) => {
 
     return (
         data.length > 0 &&
-        <section className='bg-[#161616] w-full h-[100vh] '>
+        <section className='bg-[#161616] w-full h-[100vh] max-[1024px]:h-full'>
             <div className='container mx-auto flex justify-center items-center h-full'>
-                <div className='flex'>
-                    <div className=' basis-3/4 group relative overflow-hidden'>
+                <div className='flex max-[1024px]:flex-col'>
+                    <div className=' basis-3/4 max-[1024px]:basis-2/4 group relative overflow-hidden'>
                         <div className='bg-black z-10 w-full h-full absolute opacity-0 group-hover:opacity-50  ease-in duration-300 '></div>
                         <img className='w-full h-full object-cover object-center  transform transition duration-500 group-hover:scale-105' src={`${data[0].img[0].url}`} alt="" />
                         <div className='absolute top-0 z-20 left-0 w-full h-full p-10 flex flex-col justify-end'>
-                            <p className='text-[32px] text-gray-400 group-hover:text-white transition-all font-bold w-[450px]'>{data[0].title}</p>
-                            <Link to={`/Blog/${data[0]._id}`} className='text-[20px] text-white group-hover:text-gray-400 font-semibold transition-all hover:text-blue-600 pt-[10px]'>Read More </Link>
+                            <p className='text-[32px] max-[548px]:text-[15px]  max-[768px]:w-full max-[768px]:text-[17px] text-gray-400 group-hover:text-white transition-all font-bold w-[450px]'>{data[0].title}</p>
+                            <Link to={`/Blog/${data[0]._id}`} className='text-[20px] max-[548px]:text-[12px] max-[768px]:text-[15px] text-white group-hover:text-gray-400 font-semibold transition-all hover:text-blue-600 pt-[10px]'>Read More </Link>
                         </div>
                     </div>
-                    <div className=' basis-1/4 flex flex-col'>
+                    <div className=' basis-1/4 max-[1024px]:basis-2/4 flex flex-col'>
                         <div className='group relative overflow-hidden basis-1/2'>
                             <div className='bg-black z-10 w-full h-full absolute opacity-0 group-hover:opacity-50  ease-in duration-300 '></div>
                             <img className='w-full h-full object-cover object-center  transform transition duration-500 group-hover:scale-105' src={`${data[1].img[0].url}`} alt="" />
                             <div className='absolute top-0 z-20 left-0 w-full h-full p-10 flex flex-col justify-end'>
-                                <p className='text-[17px] text-gray-400 group-hover:text-white transition-all font-bold w-[full]'>{data[1].title}</p>
-                                <Link to={`/Blog/${data[1]._id}`} className='text-[15px] text-white group-hover:text-gray-400 font-semibold transition-all hover:text-blue-600 pt-[10px]'>Read More</Link>
+                                <p className=' max-[1024px]:text-[32px] max-[548px]:text-[15px] max-[1024px]:w-[450px] max-[768px]:w-full max-[768px]:text-[17px] text-gray-400 group-hover:text-white transition-all font-bold w-[full]'>{data[1].title}</p>
+                                <Link to={`/Blog/${data[1]._id}`} className='max-[768px]:text-[15px] max-[548px]:text-[12px] max-[1024px]:text-[20px] text-white group-hover:text-gray-400 font-semibold transition-all hover:text-blue-600 pt-[10px]'>Read More</Link>
                             </div>
                         </div>
-                        <div className='group relative overflow-hidden basis-1/2'>
+                        <div className='group max-[1024px]:hidden relative overflow-hidden basis-1/2'>
                             <div className='bg-black z-10  w-full h-full absolute opacity-0 group-hover:opacity-50  ease-in duration-300 '></div>
                             <img className='w-full h-full object-cover object-center  transform transition duration-500 group-hover:scale-105' src={`${data[2].img[0].url}`} alt="" />
                             <div className='absolute top-0 z-20 left-0 w-full h-full p-10 flex flex-col justify-end'>
--- front/src/page/BlogAbout.jsx
+++ front/src/page/BlogAbout.jsx
@@ -25,7 +25,30 @@ const BlogAbout = () => {
     return (
         data &&
         <section>
-s
+            <header>
+                <div className='group h-[70vh] relative overflow-hidden'>
+                    <div className='bg-black z-10 w-full h-full absolute opacity-0 group-hover:opacity-50  ease-in duration-300 '></div>
+                    <img className='w-full h-full object-cover object-center  transform transition duration-500 group-hover:scale-105' src={`${data.img[0].url}`} alt="" />
+                    <div className='absolute top-0 z-20 left-0 w-full h-full p-10 flex flex-col justify-end'>
+                        <p className='text-[32px] max-[600px]:w-[300px] max-[600px]:text-[22px]  text-gray-400 group-hover:text-white transition-all font-bold w-[450px]'>{data.title}</p>
+                    </div>
+                </div>
+            </header>
+            <div className='container mx-auto'>
+                <section className='flex py-[60px]'>
+                    <div className='basis-2/4'>
+                        <img className='w-full h-full object-cover object-center  transform transition duration-500 group-hover:scale-105' src={`${data.img.length > 1 ? data.img[1].url : data.img[0].url}`} alt="" />
+                    </div>
+                    <div className='basis-2/4 flex justify-center items-center'>
+                        <p className='text-[26px] font-thin font-semibold text-center w-[600px]'>
+                            {data.title}
+                        </p>
+                    </div>
+                </section>
+                <section>
+                    
+                </section>
+            </div>
         </section>
     )
 }
